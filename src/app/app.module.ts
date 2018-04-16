15b4ef73b25fd81c4493d5aaef3a0d6c4df8c9ca
katzi0@gmail.com <katzi0@gmail.com>
2018-04-16 12:53:40 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularCesiumModule } from 'angular-cesium';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularCesiumModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
